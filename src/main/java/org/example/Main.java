7072e08d426966a324bcf3b3073c2cfff905ab34
Lenovo <fatiparvizi2005@gmail.com>
2025-04-24 11:06:28 +0330

    register and log in without stay logged in flag

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
+package org.example;
+
 import views.AppViews;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        new AppViews()).run();
+        (new AppViews()).run();
     }
 }
