8d6651242a0d147deda82ced3387b7f67c3125a3
SaminSadeghipour <sadeghipoursamin@gmail.com>
2025-07-08 23:19:35 +0330

    fixed run issue

--- lwjgl3/src/main/java/org/example/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/org/example/lwjgl3/Lwjgl3Launcher.java
@@ -1,4 +1,4 @@
-package com.tilldawn.lwjgl3;
+package org.example.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
