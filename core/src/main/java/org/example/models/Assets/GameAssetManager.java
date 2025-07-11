22c466e3e58db5e889f100e27cd49c2f57fc1d4d
Negar2006 <nyarahmadi2006@gmail.com>
2025-07-11 15:06:37 -0700

    delete emoji asset

--- core/src/main/java/org/example/models/Assets/GameAssetManager.java
+++ core/src/main/java/org/example/models/Assets/GameAssetManager.java
@@ -17,22 +17,6 @@ public class GameAssetManager {
     public static Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
     private static GameAssetManager gameAssetManager;
 
-    private final String character1_Emojis0 = "Emoji/Emojis000.png";
-    private final String character1_Emojis1 = "Emoji/Emojis001.png";
-    private final String character1_Emojis2 = "Emoji/Emojis002.png";
-    private final String character1_Emojis3 = "Emoji/Emojis003.png";
-    private final String character1_Emojis4 = "Emoji/Emojis004.png";
-    private final String character1_Emojis5 = "Emoji/Emojis005.png";
-    private final Texture character1_Emojis0_tex = new Texture(character1_Emojis0);
-    private final Texture character1_Emojis1_tex = new Texture(character1_Emojis1);
-    private final Texture character1_Emojis2_tex = new Texture(character1_Emojis2);
-    private final Texture character1_Emojis3_tex = new Texture(character1_Emojis3);
-    private final Texture character1_Emojis4_tex = new Texture(character1_Emojis4);
-    private final Texture character1_Emojis5_tex = new Texture(character1_Emojis5);
-    private final Animation<Texture> character1_Emojis_frames = new Animation<>(0.1f, character1_Emojis0_tex,
-        character1_Emojis1_tex, character1_Emojis2_tex, character1_Emojis3_tex, character1_Emojis4_tex, character1_Emojis5_tex);
-
-    //plants
     private final Texture LAKE_TEXTURE = new Texture("Flooring/Flooring_26.png");
     private final Texture PLANTS = new Texture("Flooring/Flooring_50.png");
     private final Texture STONE = new Texture("Rock/Farm_Boulder.png");
@@ -69,10 +53,6 @@ public class GameAssetManager {
 
     public static void dispose() {
         skin.dispose();
-    }
-
-    public Animation<Texture> getCharacter1_Emojis_animation() {
-        return character1_Emojis_frames;
     }
 
     public Texture getLAKE_TEXTURE() {
