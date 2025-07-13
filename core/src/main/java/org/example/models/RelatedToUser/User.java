0c81f9a583ac9a9a754a81bfebed9c13d18e6102
Negar2006 <nyarahmadi2006@gmail.com>
2025-07-13 12:45:54 -0700

    merge with samin

--- core/src/main/java/org/example/models/RelatedToUser/User.java
+++ core/src/main/java/org/example/models/RelatedToUser/User.java
@@ -20,10 +20,14 @@ public class User {
 
     private String answerOfQuestionForSecurity;
 
+
     private boolean isFemale;
 
-    public User() {
-    }
+    private transient String token;
+
+    private transient long tokenExpiration;
+
+    public User() {}
 
     public User(ArrayList<Game> games, String userName, String nickname, String password, String email,
                 String questionForSecurity, String answerOfQuestionForSecurity, boolean isFemale, String avatarPath) {
@@ -38,63 +42,95 @@ public class User {
         this.avatarPath = avatarPath;
     }
 
-    public void setQuestionForSecurity(String questionForSecurity) {
-        this.questionForSecurity = questionForSecurity;
+    public String getToken() {
+        return token;
     }
 
-    public void setAnswerOfQuestionForSecurity(String answerOfQuestionForSecurity) {
-        this.answerOfQuestionForSecurity = answerOfQuestionForSecurity;
+    public void setToken(String token) {
+        this.token = token;
     }
 
-    public String getUserName() {
-        return userName;
+    public long getTokenExpiration() {
+        return tokenExpiration;
     }
 
-    public String getNickname() {
-        return nickname;
+    public void setTokenExpiration(long tokenExpiration) {
+        this.tokenExpiration = tokenExpiration;
+    }
+
+    public boolean isTokenExpired() {
+        return System.currentTimeMillis() > tokenExpiration;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public String getEmail() {
-        return email;
+    public String getAvatarPath() {
+        return avatarPath;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAnswerOfQuestionForSecurity() {
-        return answerOfQuestionForSecurity;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public ArrayList<Game> getGames() {
-        return games;
-    }
-
-    public Boolean isFemale() {
-        return isFemale;
+    public String getUserName() {
+        return userName;
     }
 
     public void setAvatarPath(String avatarPath) {
         this.avatarPath = avatarPath;
     }
 
-    public String getAvatarPath() {
-        return avatarPath;
+    public ArrayList<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(ArrayList<Game> games) {
+        this.games = games;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getQuestionForSecurity() {
+        return questionForSecurity;
+    }
+
+    public void setQuestionForSecurity(String questionForSecurity) {
+        this.questionForSecurity = questionForSecurity;
+    }
+
+    public String getAnswerOfQuestionForSecurity() {
+        return answerOfQuestionForSecurity;
+    }
+
+    public void setAnswerOfQuestionForSecurity(String answerOfQuestionForSecurity) {
+        this.answerOfQuestionForSecurity = answerOfQuestionForSecurity;
+    }
+
+    public boolean isFemale() {
+        return isFemale;
+    }
+
+    public void setFemale(boolean female) {
+        isFemale = female;
     }
 }
